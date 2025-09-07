ade5ce6c12db7110316f641bf7dfd327fb6db2e3
Demo <bestfriendever742@gmail.com>
2025-09-07 20:20:16 +0600

    fix

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,8 @@
+// next.config.js
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* other config options here */
-
   eslint: {
-    // WARNING: This will ignore all ESLint errors during build
     ignoreDuringBuilds: true,
   },
 };
