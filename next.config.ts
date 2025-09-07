5b18f5d0277b1a9bfd3afc753711a50187c1d61c
Demo <bestfriendever742@gmail.com>
2025-09-07 20:14:23 +0600

    setup vercel

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  /* other config options here */
+
+  eslint: {
+    // WARNING: This will ignore all ESLint errors during build
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
