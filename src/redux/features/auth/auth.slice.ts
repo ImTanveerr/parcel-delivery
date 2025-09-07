3bc7e5d73b8fcfd340f79c4e4cf89f3a09c30db7
Demo <bestfriendever742@gmail.com>
2025-09-07 21:00:15 +0600

    fix bug

--- src/redux/features/auth/auth.slice.ts
+++ src/redux/features/auth/auth.slice.ts
@@ -1,23 +1,28 @@
+import { IUser } from "@/src/types/user.types";
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 /* eslint-disable @typescript-eslint/no-explicit-any */
 
 interface AuthState {
-  user: any | null;
+  user: IUser | null;
+  token: string | null;
 }
 
 const initialState: AuthState = {
   user: null,
+  token: null,
 };
 
 export const authSlice = createSlice({
   name: "auth",
   initialState,
   reducers: {
-    setUser: (state, action: PayloadAction<any>) => {
-      state.user = action.payload;
+    setUser: (state, action: PayloadAction<{ user: IUser; token: string }>) => {
+      state.user = action.payload.user;
+      state.token = action.payload.token;
     },
     resetUser: (state) => {
       state.user = null;
+      state.token = null;
     },
   },
 });
